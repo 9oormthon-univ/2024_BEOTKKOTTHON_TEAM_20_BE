b742b328107a98ff8dfb21c2284db97ac11be0c5
chaeeun-Han <chaeeun.h911@gmail.com>
2024-03-13 21:33:52 +0900

    chore: Add Spring Boot template

---
+++ src/main/java/com/beotkkot/qtudy/QtudyApplication.java
@@ -0,0 +1,13 @@
+package com.beotkkot.qtudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QtudyApplication.class, args);
+	}
+
+}
