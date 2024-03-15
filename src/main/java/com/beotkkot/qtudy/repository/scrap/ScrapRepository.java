c89ac38fdf354d0b19397584f351f43df871390e
chaeeun-Han <chaeeun.h911@gmail.com>
2024-03-16 02:56:17 +0900

    feat: Add `ScrapRepository` (#2)

---
+++ src/main/java/com/beotkkot/qtudy/repository/scrap/ScrapRepository.java
@@ -0,0 +1,22 @@
+package com.beotkkot.qtudy.repository.scrap;
+
+import com.beotkkot.qtudy.domain.scrap.Scrap;
+import com.beotkkot.qtudy.domain.primaryKey.ScrapPk;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface ScrapRepository extends JpaRepository<Scrap, ScrapPk> {
+    Scrap findByPostIdAndUserId(Long postId, Long uid);
+
+    @Query("SELECT s.postId FROM Scrap s WHERE s.userId = :userId")
+    List<Long> findPostIdsByUserId(@Param("userId") Long userId);
+
+    @Transactional
+    void deleteByPostId(Long postId);
+}
