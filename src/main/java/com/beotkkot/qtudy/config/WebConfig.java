5cbd590356665cbbd113d93a72085da2a62a9d55
Yeseo <chinkl0110@naver.com>
2024-03-15 20:13:36 +0900

    feat: Add cors config(#1)

---
+++ src/main/java/com/beotkkot/qtudy/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.beotkkot.qtudy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 설정
+                .allowCredentials(true); // Credential 허용 여부 설정
+        }
+}
