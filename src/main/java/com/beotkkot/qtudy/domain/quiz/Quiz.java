714bce01bc4b4be5fc5b47137faee18562af01ee
chaeeun-Han <chaeeun.h911@gmail.com>
2024-03-18 19:12:10 +0900

    feat: Add Quiz entity (#6)

---
+++ src/main/java/com/beotkkot/qtudy/domain/quiz/Quiz.java
@@ -0,0 +1,34 @@
+package com.beotkkot.qtudy.domain.quiz;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long quizId;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String tags;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String question;
+
+    @Column(/*nullable = false,*/  columnDefinition = "TEXT")
+    private String answer;
+
+    @Column(/*nullable = false,*/  columnDefinition = "TEXT")
+    private String explanation;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String options;
+}
