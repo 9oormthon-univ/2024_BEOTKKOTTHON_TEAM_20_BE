e86886eea16ca822788116e0af72bcdfb38b5fcd
Yeseo <chinkl0110@naver.com>
2024-03-20 00:48:36 +0900

    feat: Modify CommentsRepository (#11)

--- src/main/java/com/beotkkot/qtudy/repository/comments/CommentsRepository.java
+++ src/main/java/com/beotkkot/qtudy/repository/comments/CommentsRepository.java
@@ -1,11 +1,18 @@
 package com.beotkkot.qtudy.repository.comments;
 
 import com.beotkkot.qtudy.domain.comments.Comments;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
-    // int countByPostId(Long postid);
 
-//    @Transactional
-//    void deleteByPostId(Long postId);
+
+    Page<Comments> findAllByPostId(Long postId, Pageable pageable);
+
+    int countByPostId(Long postid);
+
+    @Transactional
+    void deleteByPostId(Long postId);
 }
