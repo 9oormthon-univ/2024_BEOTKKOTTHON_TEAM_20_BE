d5b2eb7664e1a3bd836772485363b4303609cd0c
chaeeun-Han <chaeeun.h911@gmail.com>
2024-03-20 17:53:52 +0900

    feat: Add `findTagsByCategoryId` (#24)

--- src/main/java/com/beotkkot/qtudy/controller/tag/TagApiController.java
+++ src/main/java/com/beotkkot/qtudy/controller/tag/TagApiController.java
@@ -1,10 +1,11 @@
 package com.beotkkot.qtudy.controller.tag;
 
-import com.beotkkot.qtudy.dto.response.tags.GetTop3TagsDto;
+import com.beotkkot.qtudy.dto.response.tags.GetTagsResponseDto;
 import com.beotkkot.qtudy.service.tag.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -13,8 +14,14 @@ public class TagApiController {
     private final TagService tagService;
 
     @GetMapping("/tag/top3")
-    public ResponseEntity<? super GetTop3TagsDto> getTop3Tags() {
-        ResponseEntity<? super GetTop3TagsDto> response = tagService.getTop3Tags();
+    public ResponseEntity<? super GetTagsResponseDto> getTop3Tags() {
+        ResponseEntity<? super GetTagsResponseDto> response = tagService.getTop3Tags();
+        return response;
+    }
+
+    @GetMapping("/tag")
+    public ResponseEntity<? super GetTagsResponseDto> getTagsByCategory(@RequestParam("categoryId") Long categoryId) {
+        ResponseEntity<? super GetTagsResponseDto> response = tagService.getTagsByCategory(categoryId);
         return response;
     }
 }
