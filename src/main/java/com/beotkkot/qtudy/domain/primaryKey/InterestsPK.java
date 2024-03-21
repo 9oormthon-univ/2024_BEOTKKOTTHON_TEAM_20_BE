67277c05aaf133b3652632cf89e1e1bbefa367f3
Yeseo <chinkl0110@naver.com>
2024-03-22 00:34:21 +0900

    feat: Add Interests entity (#17)

---
+++ src/main/java/com/beotkkot/qtudy/domain/primaryKey/InterestsPK.java
@@ -0,0 +1,21 @@
+package com.beotkkot.qtudy.domain.primaryKey;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InterestsPK implements Serializable {
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "category_id")
+    private Long categoryId;
+}
