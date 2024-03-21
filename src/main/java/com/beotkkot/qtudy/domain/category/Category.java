82d1129c5b3faaddb17f2d45705c4fb110838911
Yeseo <chinkl0110@naver.com>
2024-03-22 00:31:04 +0900

    feat: Modify Category entity (#17)

--- src/main/java/com/beotkkot/qtudy/domain/category/Category.java
+++ src/main/java/com/beotkkot/qtudy/domain/category/Category.java
@@ -1,20 +1,19 @@
 package com.beotkkot.qtudy.domain.category;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
     private String name;
