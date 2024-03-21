19b0b5a1ab226fb6bdff8dcf7f71a49006ded9b9
Yeseo <chinkl0110@naver.com>
2024-03-22 00:32:29 +0900

    feat: Add CategoryRepository (#17)

---
+++ src/main/java/com/beotkkot/qtudy/repository/category/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.beotkkot.qtudy.repository.category;
+
+import com.beotkkot.qtudy.domain.category.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
