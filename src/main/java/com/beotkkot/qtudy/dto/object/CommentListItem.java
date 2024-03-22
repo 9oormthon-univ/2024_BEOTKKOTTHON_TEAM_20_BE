b2bf2c135bfc494f671f4132be32998216cb6862
Yeseo <chinkl0110@naver.com>
2024-03-22 19:22:56 +0900

    refactor: Modify CommentService (#31)

--- src/main/java/com/beotkkot/qtudy/dto/object/CommentListItem.java
+++ src/main/java/com/beotkkot/qtudy/dto/object/CommentListItem.java
@@ -1,6 +1,7 @@
 package com.beotkkot.qtudy.dto.object;
 
 import com.beotkkot.qtudy.domain.comments.Comments;
+import com.beotkkot.qtudy.domain.user.Users;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,13 +15,17 @@ public class CommentListItem {
     private Long commentId;
     private String content;
     private String createdAt;
+    private String name;
+    private String profileImageUrl;
 
-    public static CommentListItem of(Comments comment) {
+    public static CommentListItem of(Comments comment, Users user) {
 
         return CommentListItem.builder()
                 .commentId(comment.getCommentId())
                 .content(comment.getContent())
                 .createdAt(comment.getCreatedAt())
+                .name(user.getName())
+                .profileImageUrl(user.getProfileImageUrl())
                 .build();
     }
 }
