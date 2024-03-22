30b7df82346150a95ad6d2b881ef68a885bf4111
Yeseo <chinkl0110@naver.com>
2024-03-22 14:05:47 +0900

    feat: Add DeleteCommentsResponseDto (#31)

---
+++ src/main/java/com/beotkkot/qtudy/dto/response/comments/DeleteCommentsResponseDto.java
@@ -0,0 +1,40 @@
+package com.beotkkot.qtudy.dto.response.comments;
+
+import com.beotkkot.qtudy.common.ResponseCode;
+import com.beotkkot.qtudy.common.ResponseMessage;
+import com.beotkkot.qtudy.dto.response.ResponseDto;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class DeleteCommentsResponseDto extends ResponseDto{
+    public DeleteCommentsResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    public static ResponseEntity<DeleteCommentsResponseDto> success() {
+        DeleteCommentsResponseDto result = new DeleteCommentsResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistedPost(){
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_POST, ResponseMessage.NOT_EXISTED_POST);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistedUser() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<? super DeleteCommentsResponseDto> notExistedComment() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_COMMENT, ResponseMessage.NOT_EXISTED_COMMENT);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> noPermission() {
+        ResponseDto result = new ResponseDto(ResponseCode.NO_PERMISSION, ResponseMessage.NO_PERMISSION);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(result);
+    }
+}
