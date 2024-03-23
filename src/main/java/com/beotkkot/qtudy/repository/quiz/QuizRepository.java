9906857cc932f6af5af910a1d105a3e871ae463b
chaeeun-Han <chaeeun.h911@gmail.com>
2024-03-23 19:46:13 +0900

    fix: Add `deleteByPostId` (#52)

--- src/main/java/com/beotkkot/qtudy/repository/quiz/QuizRepository.java
+++ src/main/java/com/beotkkot/qtudy/repository/quiz/QuizRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,4 +18,6 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
     @Query("SELECT q FROM Quiz q WHERE q.tags LIKE %:tagName% ORDER BY q.quizId ASC, RAND() LIMIT 10")
     List<Quiz> findByTagName(@Param("tagName") String tagName);
 
+    @Transactional
+    void deleteByPostId(Long postId);
 }
--- src/main/java/com/beotkkot/qtudy/repository/quiz/ReviewRepository.java
+++ src/main/java/com/beotkkot/qtudy/repository/quiz/ReviewRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -29,4 +30,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("SELECT r FROM Review r " +
             "WHERE r.reviewId = :reviewId")
     List<Review> findReviewByReviewId(@Param("reviewId") String reviewId);
+
+    @Transactional
+    void deleteByPostId(Long postId);
 }
