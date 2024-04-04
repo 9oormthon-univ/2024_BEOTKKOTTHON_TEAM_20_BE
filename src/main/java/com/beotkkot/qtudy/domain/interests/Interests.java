c17d7b034322fe2e42c4176a856e3716cd99817f
Yeseo <chinkl0110@naver.com>
2024-04-04 23:29:38 +0900

    refactor: Delete `@Setter` (#56)
    Co-authored-by: chaeeun-Han <chaeeun.h911@gmail.com>

--- src/main/java/com/beotkkot/qtudy/domain/interests/Interests.java
+++ src/main/java/com/beotkkot/qtudy/domain/interests/Interests.java
@@ -7,10 +7,8 @@ import jakarta.persistence.IdClass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
