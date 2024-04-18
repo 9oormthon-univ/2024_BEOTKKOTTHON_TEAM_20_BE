d94d0eac1679697dbe817004257cf7c5c3d1329c
Yeseo <chinkl0110@naver.com>
2024-04-18 20:25:08 +0900

    refactor: Delete  import (#63)

--- src/main/java/com/beotkkot/qtudy/domain/primaryKey/ScrapPk.java
+++ src/main/java/com/beotkkot/qtudy/domain/primaryKey/ScrapPk.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.io.Serializable;
 
 @Getter
